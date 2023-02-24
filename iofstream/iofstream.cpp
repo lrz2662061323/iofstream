f2be98053d20c083d59ce0b1e25afebeeb01a3f0
荣臻 刘 <2662061323@qq.com>
2023-02-24 11:29:31 +0800

    111

---
+++ iofstream/iofstream.cpp
@@ -0,0 +1,15 @@
+﻿
+
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	std::string name = "abc";
+	int age = 49;
+
+	std::ofstream fout("TEST.txt");
+
+    std::cout << "Hello World!\n";
+}
+
