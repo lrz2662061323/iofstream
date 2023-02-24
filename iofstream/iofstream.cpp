33351b8490060bc9d71f97a762b078aea280c583
荣臻 刘 <2662061323@qq.com>
2023-02-24 12:13:23 +0800

    更行

--- iofstream/iofstream.cpp
+++ iofstream/iofstream.cpp
@@ -7,9 +7,17 @@ int main()
 {
 	std::string name = "abc";
 	int age = 49;
+	char data[100];
 
-	std::ofstream fout("TEST.txt");
-
-    std::cout << "Hello World!\n";
+	std::ofstream fout;
+	fout.open("TEST.txt");
+	std::cout << "Writing to the file" << std::endl;
+	std::cout << "Enter your name: ";
+	std::cin.getline(data, 100);
+	std::cout << "Enter your age: ";
+	std::cin >> data;
+	std::cin.ignore();
+	fout.close();
+	return 0;
 }
 
